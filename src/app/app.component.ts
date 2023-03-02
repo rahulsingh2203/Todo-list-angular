fb3a00ec8a8322420a567bec871dcfc658d5c536
Rahul Singh <rahul.singh.ec.2017@miet.ac.in>
2023-03-02 13:39:28 +0530

    first commit: basic done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  list: any[] = [];
+  listLength: number = this.list.length;
+  removeItem: number = 0;
+  addTask(item: string) {
+    this.list.push({ id: this.list.length + 1, name: item });
+    console.log(this.list);
+  }
+
+  removeTask(id: number) {
+    console.log(id);
+    this.list = this.list.filter(item => item.id !== id)
+    /*
+    this.removeItem = id;
+    this.list = this.list.slice(this.removeItem,1);
+    console.log(this.list);
+    */
+  }
 }
